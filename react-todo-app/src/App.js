35412df93312dbfd58fa00b4666af7773054ba2e
youssefa123 <yabdelhady905@gmail.com>
2023-09-26 23:12:01 -0400

    Installed react, setting up project

---
+++ react-todo-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <hi> TODO app</hi>
+
+    </div>
+  );
+}
+
+export default App;
