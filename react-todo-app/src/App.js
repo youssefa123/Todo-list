b5f49e22bf961691e9159f749f9c1a75ac429aed
youssefa123 <yabdelhady905@gmail.com>
2023-09-27 10:14:04 -0400

    edited the todo componet

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
+
+
 
 function App() {
   return (
     <div className="todo-app">
-      <hi> TODO app</hi>
-
+      <TodoList/>
     </div>
   );
 }
